eff70223ac891d705d479ff03b75b22797bc8665
petermal1983 <peter1983@mail.ru>
2020-10-29 23:52:44 +0500

    Initial commit

---
+++ djangoProject/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from djangoProject.views import hello
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('djangoProject/', hello)
+]

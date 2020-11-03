a22e572f9f56deb92ed3285b56356c2caaa26fd0
petermal1983 <peter1983@mail.ru>
2020-11-03 23:29:49 +0500

    u

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from .models import Post
+#from .models import Post
 # Register your models here.
 
 #admin.site.register(Post)
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'publish', 'status')
-    list_filter = ('status', 'created', 'publish', 'author')
-    search_fields = ('title', 'body')
-    ordering = ('status', 'publish')
-    date_hierarchy = 'publish'
+#@admin.register(Post)
+#@admin.register()
+#class PostAdmin(admin.ModelAdmin):
+#    list_display = ('title', 'slug', 'author', 'publish', 'status')
+#   list_filter = ('status', 'created', 'publish', 'author')
+#    search_fields = ('title', 'body')
+#   ordering = ('status', 'publish')
+#   date_hierarchy = 'publish'
